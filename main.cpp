e0d298faeba6e63167164fb9010aa5e76527b9a3
sdekhterman <sdekhterman@gmail.com>
2024-06-26 00:16:01 -0500

    Added constructors, destructors, and user input handling (chars need to be error handled.)

--- main.cpp
+++ main.cpp
@@ -1,19 +1,42 @@
-// Your First C++ Program
-
 #include <iostream>
-#include <conio.h>
-
 using namespace std;
+
+class Doggo {
+  public:
+    void setDogHat(int hatInt);
+    int getDogHat();
+    Doggo()
+    {
+        cout<<"A dog approaches from the distance."<<endl;
+    }
+    ~Doggo()
+    {
+        cout<<"Years pass and yet the memory of the dog stays with you.";
+    }
+  private:   
+    int dogHatStatus;
+};
+
+void Doggo::setDogHat(int hatInt) {
+  dogHatStatus = hatInt;
+}
+
+int Doggo::getDogHat() {
+  return dogHatStatus;
+}
+
 int main() {
-    char s;
+    Doggo dog;
+    int userIn;
     cout << "There is a dog!" << endl;
-    cout << "Would you like to give the dog a hat? (y/n)" << endl;
-    cin >> s;
-    switch(s){
-        case 'y':
-            cout << "The dog likes the hat! <3";
+    cout << "Would you like to give the dog a hat? [yes (1)/ no (0)]" << endl;
+    cin >> userIn;
+    dog.setDogHat(userIn);
+    switch(dog.getDogHat()){
+        case 1:
+            cout << "The dog likes the hat! <3" << endl;
             break;
-        case 'n':
+        case 0:
             getchar();
             cout << "The hat is ticking?!" << endl;
             getchar();
@@ -25,7 +48,7 @@ int main() {
             getchar();
             cout << "BOOM" << endl;
             getchar();
-            cout << "You saved the dog's life.";
+            cout << "You saved the dog's life." << endl;
             break;
         default:
             cout << "The dog vanishes." << endl;
