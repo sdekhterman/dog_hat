247e58b44ca1d06f4a45773a5afaf43a9b6f05f9
Sam Dekhterman <39445510+sdekhterman@users.noreply.github.com>
2024-07-09 00:59:38 -0500

    better user input handling in main.cpp

--- main.cpp
+++ main.cpp
@@ -9,25 +9,25 @@ int main() {
     int userInt;
 
     // program navigation instructions
-    cout << " --------------------------------------------------------------------" << endl;
-    cout << "          _ __ _                              "   << endl;
-    cout << "         |      |                             "   << endl;
-    cout << "        ----------                            "   << endl;
-    cout << "          /\\___/\\                             " << endl;
-    cout << "         / o   o \\                            "  << endl;
-    cout << "        /  . @ .  \\_____________________     "   << endl;
-    cout << "        |   '''                       | \\   "    << endl;
-    cout << "        |                             | |    "    << endl;
-    cout << "        |                             | |    "    << endl;
-    cout << "         \\                ___    _    |/     "   << endl;
-    cout << "          \\   |  ________/   \\  | \\   |   d  " << endl;
-    cout << "           |__|__|           |__|  |__|  sam " << endl;
-    cout << "Press Enter to advance the text and enter an integer when prompted." << endl;
-    cout << " --------------------------------------------------------------------" << endl;
+    cout << "----------------------------------------------------------------------" << endl;
+    cout << "                    _ __ _                              " << endl;
+    cout << "                   |      |                             " << endl;
+    cout << "                  ----------                            " << endl;
+    cout << "                    /\\___/\\                           " << endl;
+    cout << "                   / o   o \\                           " << endl;
+    cout << "                  /  . @ .  \\_____________________     " << endl;
+    cout << "                  |   '''                       | \\    " << endl;
+    cout << "                  |                             | |     " << endl;
+    cout << "                  |                             | |     " << endl;
+    cout << "                   \\                ___    _    |/     " << endl;
+    cout << "                    \\   |  ________/   \\  | \\   |   d" << endl;
+    cout << "                     |__|__|           |__|  |__|  sam  " << endl << endl;
+    cout << "Press 1 for yes and 2 for no when prompted. 'Enter' advances the text." << endl;
+    cout << "----------------------------------------------------------------------" << endl;
 
-    // instruct the user, get their input, and store it
+    //  instruct the user, get their input, and store it
     cout << "There is a dog!" << endl;
-    cout << "Would you like to give the dog a hat? [yes (1)/ no (0)]" << endl;
+    cout << "Would you like to give the dog a hat? [1 or 2]" << endl;
     cin >> userInt;
     dog.setDogHat(userInt);
 
@@ -36,7 +36,7 @@ int main() {
         case 1:
             cout << "The dog likes the hat! <3" << endl;
             break;
-        case 0:
+        case 2:
             getchar();
             cout << "The hat is ticking?!" << endl;
             getchar();
