933860b7cb1da6d30fdb8aad25feb5e11ec7aa3e
Sam Dekhterman <39445510+sdekhterman@users.noreply.github.com>
2024-07-09 00:29:12 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,24 @@ int main() {
     Doggo dog;
     int userInt;
 
-    // instruct the user, get their input and store it
+    // program navigation instructions
+    cout << " --------------------------------------------------------------------" << endl;
+    cout << "          _ __ _                              "   << endl;
+    cout << "         |      |                             "   << endl;
+    cout << "        ----------                            "   << endl;
+    cout << "          /\\___/\\                             " << endl;
+    cout << "         / o   o \\                            "  << endl;
+    cout << "        /  . @ .  \\_____________________     "   << endl;
+    cout << "        |   '''                       | \\   "    << endl;
+    cout << "        |                             | |    "    << endl;
+    cout << "        |                             | |    "    << endl;
+    cout << "         \\                ___    _    |/     "   << endl;
+    cout << "          \\   |  ________/   \\  | \\   |   d  " << endl;
+    cout << "           |__|__|           |__|  |__|  sam " << endl;
+    cout << "Press Enter to advance the text and enter an integer when prompted." << endl;
+    cout << " --------------------------------------------------------------------" << endl;
+
+    // instruct the user, get their input, and store it
     cout << "There is a dog!" << endl;
     cout << "Would you like to give the dog a hat? [yes (1)/ no (0)]" << endl;
     cin >> userInt;
